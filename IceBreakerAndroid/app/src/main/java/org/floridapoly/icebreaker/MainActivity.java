7af2a721c85f8e641127ce82cc8d5f1d74000679
Britt Mathis <britt.mathis@gmail.com>
2015-02-21 16:22:14 -0500

    formatting

--- IceBreakerAndroid/app/src/main/java/org/floridapoly/icebreaker/MainActivity.java
+++ IceBreakerAndroid/app/src/main/java/org/floridapoly/icebreaker/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button ;
+import android.widget.Button;
 import android.widget.TextView;
 import com.estimote.sdk.Region;
 import com.estimote.sdk.BeaconManager;
