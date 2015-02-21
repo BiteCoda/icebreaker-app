a5f751b65232f23a03bb49d956d635f17738f779
Britt Mathis <britt.mathis@gmail.com>
2015-02-21 14:13:46 -0500

    Revert "Initial Commit IOS"
    This reverts commit 1ace6ffb4ae9c30394d25e38f102c95a540822fd.

--- IceBreaker-iOS/IceBreaker/Estimote-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Estimote-Bridging-Header.h
-//  IceBreaker
-//
-//  Created by Jacob Chen on 2/21/15.
-//  Copyright (c) 2015 floridapoly.IceMakers. All rights reserved.
-//
-
-#import <EstimoteSDK/ESTBeaconManager.h>
