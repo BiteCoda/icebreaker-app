9e151df359a6b1c2616d92770d7fed347bfd77fd
Britt Mathis <britt.mathis@gmail.com>
2015-02-22 01:43:42 -0500

    working towards communication with server

--- IceBreakerAndroid/mobile/src/main/java/org/floridapoly/icebreakerandroid/MainActivity.java
+++ IceBreakerAndroid/mobile/src/main/java/org/floridapoly/icebreakerandroid/MainActivity.java
@@ -6,9 +6,30 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.content.Context;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 
+import android.util.Log;
+
+import java.io.IOException;
 
 public class MainActivity extends ActionBarActivity {
+
+    // tag used for logging purposes
+    static final String TAG = "IceBreaker";
+
+    // request code given when calling startActivityForResult
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    // project number used as sender_id
+    String SENDER_ID = "835233388096";
+
+    GoogleCloudMessaging gcm;
+    Context context;
+
+    // our registration id
+    String regid;
 
     private ProgressBar spinner;
 
@@ -16,10 +37,32 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // get our application context
+        context = getApplicationContext();
+        // we need a valid play services apk to get push notifications
+        if (checkPlayServices()) {
+            gcm = GoogleCloudMessaging.getInstance(this);
+            // our server is stateless; create new regid every time
+            // this is blocking, so we'll see if it causes problems
+            try {
+                regid = gcm.register(SENDER_ID);
+            } catch (IOException ex) {
+                Log.i(TAG, "IOException when registering: " + ex.getMessage());
+            }
+        } else {
+            Log.i(TAG, "No valid Google Play Services APK found.");
+        }
+        // spinning progress bar animation during search
         spinner = (ProgressBar) findViewById(R.id.progressBar);
         spinner.setVisibility(View.GONE);
     }
 
+    // we need to check for play services apk here too
+    @Override
+    protected void onResume() {
+       super.onResume();
+       checkPlayServices();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,12 +77,10 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -47,5 +88,25 @@ public class MainActivity extends ActionBarActivity {
         // here's where we'll start searching, etc.
         spinner.setVisibility(View.VISIBLE);
 
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 }
---
+++ IceBreakerAndroid/mobile/src/main/java/org/floridapoly/icebreakerandroid/Server.java
@@ -0,0 +1,23 @@
+package org.floridapoly.icebreakerandroid;
+
+import com.loopj.android.http.*;
+import java.util.HashMap;
+
+public class Server {
+    AsyncHttpClient client = new AsyncHttpClient();
+    public void register(String regid) {
+        // to register with the server, we need to send it a json post
+        // of a HashMap containing our major and minor beacon ids
+        // our registration id with GCM
+        // and our device type (android)
+        // (major:31782, minor:36689)
+        // http://icebreaker.duckdns.org
+        RequestParams paramMap = new RequestParams();
+        paramMap.put("userId", "(major:(31782), minor:(36689))");
+        paramMap.put("deviceToken", regid);
+        paramMap.put("deviceType", "android");
+        client.post("http://icebreaker.duckdns.org",
+                paramMap,
+                new JsonHttpResponseHandler());
+    }
+}
